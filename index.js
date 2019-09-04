13be90936dfb7ef4004d054eb50f883953b4d42e
prrrcl <adrianporcelnavarro@hotmail.com>
2019-09-04 13:02:04 +0200

    Server Created

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import express from 'express';
+// graphql
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
+
 
 const app = express();
 
 app.get('/', (req,res,next)=>{
   res.send('Todo ok!')
 })
+
+
+// resolver query
+
+const root = {
+  hola: () => 'hola mundo desde GraphQL'
+}
+
+app.use('/graphql', graphqlHTTP({
+  // pasamos el schema
+  schema,
+  // el resolver se pasa como rootValue
+  rootValue: root,
+  // utilizar graphiql
+  graphiql: true
+}))
 
 app.listen(5000, ()=> console.log('Server On'))
